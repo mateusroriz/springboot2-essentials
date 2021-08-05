1146f24e4ef52be9812bf0ad97d1930affa1954b
Mateus.Roriz <mateus.roriz@sicoob.com.br>
2021-08-05 20:27:04 -0300

    Adicionando metodo get quando um id é passado

--- src/main/java/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/devdojo/springboot2/service/AnimeService.java
@@ -1,7 +1,9 @@
 package devdojo.springboot2.service;
 
 import devdojo.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -9,7 +11,18 @@ import java.util.List;
 //aqui acontece a logica de negocios
 public class AnimeService  {
     //private final AnimeRespository animeRepository;
+
+    private List<Anime> animes = List.of(new Anime(1L,"Nagatoro"), new Anime(2L,"JOJO"));
+
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"Nagatoro"), new Anime(2L,"JOJO"));
+        return animes;
+    }
+
+
+    public Anime findById(long id){ //procurar anime pelo id
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime ID not found"));
     }
 }
