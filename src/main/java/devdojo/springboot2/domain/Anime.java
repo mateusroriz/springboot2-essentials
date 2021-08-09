f763cc70b5ad7a0ec731ea0edfcb432fb4707b4a
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-08-09 20:13:18 -0300

    adicionando spring data jpa

--- src/main/java/devdojo/springboot2/domain/Anime.java
+++ src/main/java/devdojo/springboot2/domain/Anime.java
@@ -2,11 +2,21 @@ package devdojo.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 //representa o que temos no banco de dados
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
