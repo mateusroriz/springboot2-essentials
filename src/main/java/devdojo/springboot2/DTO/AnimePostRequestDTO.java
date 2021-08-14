64ca5caf7e1cdaa4d25cb4e675f72b7750a399a6
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-08-14 14:54:27 -0300

    adicionando validação de campos no nome do anime

--- src/main/java/devdojo/springboot2/DTO/AnimePostRequestDTO.java
+++ src/main/java/devdojo/springboot2/DTO/AnimePostRequestDTO.java
@@ -2,7 +2,11 @@ package devdojo.springboot2.DTO;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnimePostRequestDTO {
+    @NotEmpty(message = "the name cannot be empty")
     private String name;
 }
