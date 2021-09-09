c46f09702c0c946e6d1fc70e4fc161fdb613b760
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-09-09 20:57:21 -0300

    adicionando tags de informações na documentação usando springdoc open api

--- src/main/java/devdojo/springboot2/DTO/AnimePostRequestDTO.java
+++ src/main/java/devdojo/springboot2/DTO/AnimePostRequestDTO.java
@@ -1,5 +1,6 @@
 package devdojo.springboot2.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,5 +14,6 @@ import javax.validation.constraints.NotEmpty;
 @NoArgsConstructor
 public class AnimePostRequestDTO {
     @NotEmpty(message = "the name cannot be empty")
+    @Schema(description = "this is the Anime's name", example = "Vinland Saga", required = true)
     private String name;
 }
