b0a3edf32b0e3f29e7a4780b44b0301aed748072
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-09-08 21:46:41 -0300

    adicionando documentação com springdoc openapi

--- src/main/java/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/devdojo/springboot2/controller/AnimeController.java
@@ -5,8 +5,10 @@ import devdojo.springboot2.DTO.AnimePostRequestDTO;
 import devdojo.springboot2.DTO.AnimePutRequestDTO;
 import devdojo.springboot2.service.AnimeService;
 import devdojo.springboot2.util.DateUtil;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -31,7 +33,7 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<Page<Anime>> list(Pageable pageable){
+    public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable){
         return  ResponseEntity.ok(animeService.listAll(pageable));
     }
 
